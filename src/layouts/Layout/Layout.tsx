655e1a496297eee50ddeadabb7408ea22e9dec27
mishina naoya <msnyxx811@gmail.com>
2024-05-01 23:36:20 +0900

    サインアップのコンポーネント作成

--- src/layouts/Layout/Layout.tsx
+++ src/layouts/Layout/Layout.tsx
@@ -1,12 +1,13 @@
 import { ResponsiveLayout, useResponsiveLayoutContext } from '@/components/layouts/ResponsiveLayout';
 import { withAuth } from '@/contexts/auth';
+import { Title } from '../_components/Title';
 import { AccountMenu } from './_components/AccountMenu';
 import type { ReactNode } from 'react';
 
 export const Layout = withAuth(({ children }: { children: ReactNode }) => {
   return (
     <ResponsiveLayout
-      header={{ title: 'Firebaseチュートリアル' }}
+      header={{ title: <Title /> }}
       navbar={{ navMenu: 'ナビメニュー', accountMenu: <AccountMenu/> }}
     >
       {children}
