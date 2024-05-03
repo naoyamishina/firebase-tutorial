c3ce618f88bb602856535da525a39b148699fb47
mishina naoya <msnyxx811@gmail.com>
2024-05-03 16:39:23 +0900

    サインインのフォーム実装

---
+++ src/components/pages/SignIn/SignIn.tsx
@@ -0,0 +1,10 @@
+import { Stack } from '@mantine/core';
+import { SignInWithEmailAndPasswordForm } from '@/components/forms/auth/SignInWithEmailAndPassword';
+
+export const SignIn = () => {
+  return (
+    <Stack maw={500} mx='auto' py='lg'>
+      <SignInWithEmailAndPasswordForm />
+    </Stack>
+  );
+};
--- src/utils/firebase/auth/auth.ts
+++ src/utils/firebase/auth/auth.ts
@@ -3,12 +3,16 @@ import {
   createUserWithEmailAndPassword as _createUserWithEmailAndPassword,
   getAuth,
   getIdTokenResult,
+  signInWithEmailAndPassword as _signInWithEmailAndPassword,
   signOut as _signOut,
 } from 'firebase/auth';
 import type { User, ParsedToken } from 'firebase/auth';
 
 const createUserWithEmailAndPassword = async (email: string, password: string) =>
   _createUserWithEmailAndPassword(getAuth(), email, password);
+
+const signInWithEmailAndPassword = async (email: string, password: string) =>
+  _signInWithEmailAndPassword(getAuth(), email, password);
 
 const signOut = async () => _signOut(getAuth());
 
@@ -17,5 +21,6 @@ export {
   createUserWithEmailAndPassword,
   getAuth,
   getIdTokenResult,
+  signInWithEmailAndPassword,
   signOut
 };
