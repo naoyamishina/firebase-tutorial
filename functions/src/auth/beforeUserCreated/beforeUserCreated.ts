62979dfb1040f5bf503aa6ed0f7b5e6e006c17ad
mishina naoya <msnyxx811@gmail.com>
2024-05-06 10:57:57 +0900

    管理者画面の作成

--- functions/src/auth/beforeUserCreated/beforeUserCreated.ts
+++ functions/src/auth/beforeUserCreated/beforeUserCreated.ts
@@ -1,8 +1,10 @@
-import { createUser, userRef } from '../../modals/user';
+import { createUser, userRef } from '../../models/user';
 import { HttpsError, beforeUserCreated as _beforeUserCreated } from '../../utils/firebase';
 
 export const beforeUserCreated = _beforeUserCreated(async (event) => {
   const user = event.data;
+  if (event.eventType === 'providers/cloud.auth/eventTypes/user.beforeCreate:google.com')
+    throw new HttpsError('unauthenticated', 'Unregistered user.');
   if (!user.email) throw new HttpsError('invalid-argument', 'Email is required.');
 
   await createUser(userRef(user.uid), { email: user.email, role: 'user' });
